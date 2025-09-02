4fb4544f3f914f019a3f6a469838f94e42f96b97
Hrusikesh123 <hrushikeshs113@gmail.com>
2025-09-02 22:54:27 +0530

    first-commit

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
+// Connect to the MongoDB database
 
-// connect to mongoDb database
+const connectDB = async () => {
+    mongoose.connection.on('connected', () => console.log('Database Connected'))
 
-const connectDB = async ()=>{
-    mongoose.connection.on('connected', ()=> console.log('Database connected successfully!')
-    )
-    await mongoose.connect(`${process.env.MONGODB_URI}/Edemy`)
-
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
 }
+
 export default connectDB;
